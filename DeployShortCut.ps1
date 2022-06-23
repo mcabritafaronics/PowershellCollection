69496a91b7fe6e3280fef9782d118bda0d8c3e25
mcabritafaronics <104034655+mcabritafaronics@users.noreply.github.com>
2022-06-23 09:41:58 -0700

    Random

---
+++ DeployShortCut.ps1
@@ -0,0 +1,7 @@
+﻿# Powershell script to copy a shortcut to all users
+$TargetFile = read-host  -Prompt "Location of shortcut's.exe"
+$ShortcutFile = read-host -Prompt "Location of shortcut .lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
---
+++ autologon.ps1
@@ -0,0 +1,65 @@
+﻿#Variables
+
+$autologon = "C:\Temp\autologon"
+$username = read-host -prompt "Enter username for autologon"
+$domain = read-host -prompt "Enter domain name for autologon"
+$password = Read-Host -Prompt "Enter Password in cleartxt"
+$FolderName = "c:\temp\autologon"
+$FileName = "C:\temp\autologon.exe"
+$Url = 'https://download.sysinternals.com/files/AutoLogon.zip' 
+$ZipFile = 'C:\Temp\' + $(Split-Path -Path $Url -Leaf) 
+$Destination= 'C:\Temp\autologon' 
+
+
+
+
+
+
+
+#Create Extraction folders
+
+
+if(Get-Item -Path $FolderName -ErrorAction Ignore)
+{
+   
+        Write-Host "Folder Exists"
+        #Create new file
+        if(Get-Item -Path $FileName -ErrorAction Ignore){
+              Write-Host "File already exists"
+        }
+        else
+        {
+            New-Item -Verbose $FileName -ItemType File
+           
+        }
+}
+else
+{
+    Write-Host "Folder Doesn't Exists"
+    
+    #PowerShell Create directory if not exists
+    New-Item $FolderName -ItemType Directory
+}
+
+#Download autologon
+
+
+$Url = 'https://download.sysinternals.com/files/AutoLogon.zip' 
+$ZipFile = 'C:\Temp\' + $(Split-Path -Path $Url -Leaf) 
+$Destination= 'C:\Temp\autologon' 
+ 
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
+ 
+$ExtractShell = New-Object -ComObject Shell.Application 
+$Files = $ExtractShell.Namespace($ZipFile).Items() 
+$ExtractShell.NameSpace($Destination).CopyHere($Files) 
+Start-Process $Destination
+
+# Execute Autologon
+
+
+
+Start-Process $autologon -ArgumentList $username,$domain,$password
+
+
+
